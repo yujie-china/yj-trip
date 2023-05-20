465f025d233be26397570940ccd5a81fca48f594
yujie-china <112388114+yujie-china@users.noreply.github.com>
2023-05-20 17:09:02 +0800

    详细补充

--- jie-trip/src/App.vue
+++ jie-trip/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { useRoute } from 'vue-router';
 import tabBar from './components/tab-bar/tab-bar.vue';
+import loading from './components/loading/loading.vue';
 const route=useRoute()
 </script>
 
 <template>
     <div class="app">
+      <loading/>
       <RouterView />
       <tabBar v-if="!route.meta.hideTabBar" />
     </div>
